f428b533cf4f9a8d4a2c30585675b6ec33f5a27a
Mr. Minich <cminich@wyoarea.org>
2018-08-24 23:18:30 -0400

    initial post

---
+++ HelloWorldWIthMethods.java
@@ -0,0 +1,26 @@
+public class HelloWorldWithMethods 
+{
+
+    public static void main(String[] args) 
+    {
+        sayHello();
+        sayWeather();
+        sayGoodbye();
+    }
+    
+    public static void sayHello() 
+    {
+        System.out.println("it's a beautiful day");
+    } 
+
+    public static void sayWeather() 
+    {
+        System.out.println("it's a beautiful day");
+    } 
+
+    public static void sayGoodbye()
+    {
+        System.out.println("goodbye");
+    {
+
+}
